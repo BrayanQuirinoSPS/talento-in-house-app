2d3ed8b5e552b80d58c28d47e23d7668391eb4ef
BrayanQuirinoSPS <bquirino@spsolutions.com.mx>
2020-12-02 11:28:06 -0600

    Prueba con secure

--- Front/src/setupProxy.js
+++ Front/src/setupProxy.js
@@ -6,6 +6,7 @@ module.exports = function(app) {
     createProxyMiddleware({
       target: 'https://anypoint.mulesoft.com/mocking/api/v1/sources/exchange/assets/d4788f6c-c024-43ee-8253-b139c8997e9e/usuarios/1.0.0/m',
       changeOrigin: true,
+      secure: false
     })
   );
 };
